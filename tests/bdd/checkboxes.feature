10bf9de8cda268e2f3d47582964f727b3ef96af9
Ned Wilbur <Ned612@Gmail.com>
2022-02-01 12:13:04 -0500

    Added bdd test 'Toggle Checkboxes'

--- tests/bdd/checkboxes.feature
+++ tests/bdd/checkboxes.feature
@@ -1,4 +1,9 @@
 Feature: Checkboxes
 
   Scenario: Toggle Checkboxes
-    Given I open TestCafe demo page
+    Given I open checkboxes
+    When I click some checkboxes
+    | CheckboxIndex |
+    | 0 |
+    | 1 |
+    Then the amount of selected checkboxes is "1"
--- tests/bdd/checkboxes.ts
+++ tests/bdd/checkboxes.ts
@@ -1,14 +1,24 @@
 import { Selector, t } from 'testcafe';
 import { Given, When, Then } from '@cucumber/cucumber';
+import Page from '../../pages/_pages';
 
-Given('I open TestCafe demo page', async t => {
-   await t.navigateTo('https://devexpress.github.io/testcafe/example/');
+Given('I open checkboxes', async t => {
+   await t.navigateTo('http://the-internet.herokuapp.com/checkboxes');
 });
 
-When('test1', async t => {
-   console.log('1');
- });
-   
- Then('test2', async t => {
-   console.log('2');
- });
+When('I click some checkboxes', async (t, [], table) => {
+  for (const { CheckboxIndex } of table.hashes()) {
+    if (!CheckboxIndex) return;
+    await t.click(Page.CheckBoxes.El.Checkboxes.nth(+CheckboxIndex));
+  }
+});
+
+Then(/^the amount of selected checkboxes is "(.+)"$/, async (t, [amount]) => {
+  const selectedCheckboxes = Page.CheckBoxes.El.Checkboxes.filter(checkbox =>
+    Boolean(checkbox && (checkbox as HTMLInputElement).checked)
+  );
+
+  const checkedCount = await selectedCheckboxes.count;
+
+  await t.expect(checkedCount).eql(Number(amount));
+});
